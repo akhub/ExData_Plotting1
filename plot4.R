dce7d8562149d2bdf220534735309bc310d70353
akhub <32000967+akhub@users.noreply.github.com>
2017-12-14 23:38:58 -0600

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -23,7 +23,7 @@ par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 # Draw plot
 with(data, { plot(Global_active_power~date_time, type="l", 
                ylab="Global Active Power (kilowatts)", xlab="")
-             plot(Voltage~date_time, type="l", ylab="Voltage (volt)", xlab="")
+             plot(Voltage~date_time, type="l", ylab="Voltage (volt)", xlab="datetime")
              plot(Sub_metering_1 ~ date_time, type = "l", 
                 ylab = "Energy sub metering", xlab = "")
             lines(Sub_metering_2~date_time,col='Red')
@@ -32,7 +32,7 @@ with(data, { plot(Global_active_power~date_time, type="l",
             legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
                    legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
             plot(Global_reactive_power~date_time, type="l", 
-                ylab="Global Rective Power (kilowatts)",xlab="")})
+                ylab="Global Rective Power (kilowatts)",xlab="datetime")})
 
 # Close png file device
 dev.off()
